02f37c7fa04d18518a1d9068f3e70057f954b350
Sudoer <sudoer.zero@gmail.com>
2022-02-28 10:34:10 +0200

    Create message model and added to admin.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'email',
+        'created_at',
+    ]
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
