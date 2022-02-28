f14f26a82bb7f90390057ceeaf4fb5360ce7a7c2
Sudoer <sudoer.zero@gmail.com>
2022-02-28 10:06:41 +0200

    Add logo and navigation bar.

---
+++ front_end/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-5/6 mx-auto py-4 border-b-2 border-slate-300">
+    <div class="grid grid-cols-2 mx-4">
+      <div class="col-span-1">
+        <img class="w-40" src="../assets/logo.svg" alt="logo">
+      </div>
+      <div class="col-span-1 font-display justify-self-end pt-3">
+        <span class="px-4">
+          <router-link to="">APPS</router-link>
+        </span>
+        <span class="px-4">
+          <router-link to="">LOGOS</router-link>
+        </span>
+        <span class="px-4">
+          <router-link to="">BLOG</router-link>
+        </span>
+        <span class="px-4">
+          <router-link to="">ABOUT</router-link>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
