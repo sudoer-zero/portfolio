4a3554eb1bf1432b128c8cccfa37818800eb6f94
Sudoer <sudoer.zero@gmail.com>
2022-03-20 09:34:07 +0200

    axios and vue axios   config.

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 import './index.css'
 
-createApp(App).use(router).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
+
+createApp(App).use(router, VueAxios, axios).mount('#app')
