63aba60999cc69281a70a25a287c20de32efdb66
Sudoer <sudoer.zero@gmail.com>
2022-03-20 10:17:00 +0200

    Messages list and django contrib auth urls.

--- back_end/urls.py
+++ back_end/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from contact.views import MessageListView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MessageListView.as_view()),
+    path('users/', include('django.contrib.auth.urls')),
     path('contact/', include('contact.urls')),
 ]
