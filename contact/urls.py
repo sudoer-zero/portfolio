e0910c25152e5a671b86a26ad19200e6cc8bfabc
Sudoer <sudoer.zero@gmail.com>
2022-03-20 10:18:18 +0200

    Message list and detail and it's urls.

--- contact/urls.py
+++ contact/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path(
         "",
         views.MessagePost.as_view()
-    )
+    ),
+
+    path(
+        '<int:pk>/',
+        views.MessageDetailView.as_view(),
+        name='message_detail'
+    ),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,9 @@
 from .serializers import MessageSerializer
 from rest_framework import mixins
 from rest_framework import generics
+from .models import Message
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class MessagePost(mixins.CreateModelMixin, generics.GenericAPIView):
@@ -8,3 +11,15 @@ class MessagePost(mixins.CreateModelMixin, generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+
+class MessageListView(LoginRequiredMixin, ListView):
+    model = Message
+    template_name = 'home.html'
+    login_url = '/users/login/'
+
+
+class MessageDetailView(LoginRequiredMixin, DetailView):
+    model = Message
+    template_name = 'contact/detail.html'
+    login_url = '/users/login/'
