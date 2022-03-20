897997059c571aa28c2041e2812e96a3fb8702f6
Sudoer <sudoer.zero@gmail.com>
2022-03-20 09:32:45 +0200

    Home and about page view.

--- front_end/src/views/AboutView.vue
+++ front_end/src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <p>About</p>
 </template>
 
 <script>
--- front_end/src/views/HomeView.vue
+++ front_end/src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <template>
-
+  <Contact/>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Contact from "@/components/Contact.vue"
 export default {
   name: 'HomeView',
   components: {
-    
+    Contact,
   }
 }
 </script>
