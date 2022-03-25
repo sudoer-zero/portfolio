e4dd304862358613d2e1dbd2d48812027702218a
Sudoer <sudoer.zero@gmail.com>
2022-03-25 21:45:17 +0200

    Add mirza font adn the color scheme.

--- front_end/tailwind.config.js
+++ front_end/tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     extend: {
       fontFamily: {
         'display':['Open Sans Condensed'],
+        'paragraph':['Mirza'],
+      },
+      colors: {
+        'first': '#F6F5F5',
+        'second': '#145374',
+        'third': '#00334E',
+        'fourth': '#EE6F57',
       }
     },
   },
