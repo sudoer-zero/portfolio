8f3c355cd12445b5d0b96a603cb9f5c801f924f5
Sudoer <sudoer.zero@gmail.com>
2022-03-26 11:54:35 +0200

    Change axios baseurl to sudoer.pythonanywhere.com

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -6,6 +6,6 @@ import VueAxios from 'vue-axios'
 import router from './router'
 import './index.css'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'https://sudoer.pythonanywhere.com/';
 
 createApp(App).use(router, VueAxios, axios).mount('#app')
