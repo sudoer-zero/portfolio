e3f584b53a67de37a6dd9f27092aa4c6fe9dfaf1
Sudoer <sudoer.zero@gmail.com>
2022-04-14 14:51:55 +0200

    Create a project model.

--- work/models.py
+++ work/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from autoslug import AutoSlugField
+from back_end.settings import localhost
 
-# Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title', unique_with='description')
+    description = models.CharField(max_length=250)
+    url = models.URLField()
+    image = models.ImageField(upload='work/covers', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+    
+    def get_image(self):
+        if self.image:
+            return localhost + self.image.url
+        return ''
