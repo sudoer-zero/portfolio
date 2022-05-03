96b9144bd10af60018a19615d54af75eaf0dedd3
Sudoer <sudoer.zero@gmail.com>
2022-05-03 21:29:48 +0200

    Configure axios

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://sudoer.pythonanywhere.com/';
 
 Vue.config.productionTip = false
 
