27ccc56ba2fbdcec688eecf7ea0957830017a10f
Sudoer <sudoer.zero@gmail.com>
2022-05-04 00:01:13 +0200

    Design improvements.

--- front_end/src/components/LogoDesign.vue
+++ front_end/src/components/LogoDesign.vue
@@ -2,15 +2,15 @@
   <div>
     <v-row align="center" justify="center" class="pb-4">
       <v-chip class=""> Logo Design </v-chip>
-      <v-col class="text-center" cols="12">
+      <v-col class="text-center px-0" cols="12">
         <h1 class="text-h2 font-weight-light mb-4">Design Meet Beauty</h1>
         <div class="grey--text mx-4">
           <p class="mb-0">
-            Explore a beautiful logo design. Where Designer mix design with
+            Explore a beautiful logo design. Where The Designer mix design with
             beauty.
           </p>
           <p>
-            You have the ability to boost your branch identity, HIRE ME NOW!
+            You have the ability to boost your brand identity, HIRE ME NOW!
           </p>
         </div>
         <div>
@@ -24,7 +24,7 @@
         </div>
       </v-col>
     </v-row>
-    <v-parallax height="300" src="../assets/img/logo-design-banner.png">
+    <v-parallax class="px-0" height="300" src="../assets/img/logo-design-banner.png">
     </v-parallax>
   </div>
 </template>
