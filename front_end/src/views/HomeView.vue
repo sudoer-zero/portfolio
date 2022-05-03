17804c05e7c8dd88d1968ce991bf2ab5beb53be0
Sudoer <sudoer.zero@gmail.com>
2022-05-04 00:05:01 +0200

    Add recentlogos component to the home page && add title.

--- front_end/src/views/HomeView.vue
+++ front_end/src/views/HomeView.vue
@@ -2,19 +2,25 @@
   <div>
     <Hero />
     <LogoDesign /> 
+    <RecentLogos />
   </div>
 </template>
 
 <script>
 import Hero from "@/components/Hero.vue";
 import LogoDesign from "@/components/LogoDesign.vue";
-
+import RecentLogos from "@/components/RecentLogos.vue";
 export default {
   name: "HomeView",
 
   components: {
     Hero,
     LogoDesign,
+    RecentLogos,
+  },
+
+  mounted() {
+    document.title = "Sudoer | Home";
   },
 };
 </script>
