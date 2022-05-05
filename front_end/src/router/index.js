cdac583242d34c846787a3994d94fc6db3faff51
Sudoer <sudoer.zero@gmail.com>
2022-05-06 01:21:11 +0200

    Add routs for blog and work pages.

--- front_end/src/router/index.js
+++ front_end/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Logo from '../views/Logo.vue'
+import Blog from '../views/Blog.vue'
+import Work from '../views/Work.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,11 +16,18 @@ const routes = [
   {
     path: '/logo',
     name: 'logo',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: Logo
-  }
+  },
+  {
+    path: '/blog',
+    name: 'blog',
+    component: Blog
+  },
+  {
+    path: '/work',
+    name: 'work',
+    component: Work
+  },
 ]
 
 const router = new VueRouter({
