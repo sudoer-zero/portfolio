32462ead5e5c869bb9dc8caa8db48565cc080184
Sudoer <sudoer.zero@gmail.com>
2022-05-06 01:21:48 +0200

    Create pages for work and blog.

---
+++ front_end/src/views/Blog.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-parallax dark src="../assets/img/blog-main-banner.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="text-h4 text-md-h2 font-weight-light mb-4">Articles</h1>
+          <div class="mx-4">
+          <p class="mb-0">
+            Explore a beautiful logo design. Where The Designer mix design with
+            beauty.
+          </p>
+          <p>
+            You have the ability to boost your brand identity, HIRE ME NOW!
+          </p>
+        </div>
+          <About />
+          <ContactForm />
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue"
+import About from "@/components/About.vue"
+
+export default {
+  name: 'BlogList',
+
+  components: {
+    ContactForm,
+    About,
+  },
+
+  mounted() {
+    document.title = "Sudoer | Blog"
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ front_end/src/views/Work.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-parallax dark src="../assets/img/work-main-banner.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="text-h4 text-md-h2 font-weight-light mb-4">Projects</h1>
+          <div class="mx-4">
+          <p class="mb-0">
+            These some of my projects I have created and some contributed to.
+          </p>
+          <p>
+            If you have an idea for a project feel free to CONTACT ME NOW!
+          </p>
+        </div>
+          <div>
+          <v-btn
+            href="https://github.com/sudoer-zero"
+            target="_black"
+            class="mx-3"
+            dark
+            ><v-icon left> mdi-github </v-icon>Github</v-btn
+          >
+         <ContactForm /> 
+        </div>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue"
+export default {
+  name: 'ProjectsList',
+
+  components: {
+    ContactForm,
+  },
+
+  mounted() {
+    document.title = "Sudoer | Work"
+  }
+
+}
+</script>
+
+<style>
+
+</style>
