69963b4c1a92e3e758be18041eec17a780fac7b4
Sudoer <sudoer.zero@gmail.com>
2022-05-05 02:12:12 +0200

    Create Loading circle component.

---
+++ front_end/src/components/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row v-show="isLoading" align="center" justify="center">
+    <v-progress-circular indeterminate color="primary"></v-progress-circular>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Loading-Circle",
+
+  props: {
+    isLoading: Boolean,
+  },
+};
+</script>
+<style scoped>
+.v-progress-circular {
+  margin: 1rem;
+}
+</style>
