5972886a11fee604077105b0a3e73c9e8f355c50
Sudoer <sudoer.zero@gmail.com>
2022-05-07 14:19:43 +0200

    Implement vue showdown.

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -3,11 +3,21 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import VueShowdown from 'vue-showdown'
 import axios from 'axios'
 
+// axios.defaults.baseURL = 'http://127.0.0.1:8000';
 axios.defaults.baseURL = 'https://sudoer.pythonanywhere.com/';
-
 Vue.config.productionTip = false
+
+Vue.use(VueShowdown, {
+  // set default flavor of showdown
+  flavor: 'original',
+  // set default options of showdown (will override the flavor options)
+  options: {
+    emoji: true,
+  },
+});
 
 new Vue({
   router,
