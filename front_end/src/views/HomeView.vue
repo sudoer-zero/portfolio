f7c566c9999bb5d0c274cad69eaacfe6c88f8f39
Sudoer <sudoer.zero@gmail.com>
2022-05-07 14:20:07 +0200

    Add recent articles component to home page.

--- front_end/src/views/HomeView.vue
+++ front_end/src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <Hero />
     <LogoDesign /> 
     <RecentLogos />
+    <RecentArticles />
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import Hero from "@/components/Hero.vue";
 import LogoDesign from "@/components/LogoDesign.vue";
 import RecentLogos from "@/components/RecentLogos.vue";
+import RecentArticles from "@/components/RecentArticles.vue";
 export default {
   name: "HomeView",
 
@@ -17,6 +19,7 @@ export default {
     Hero,
     LogoDesign,
     RecentLogos,
+    RecentArticles,
   },
 
   mounted() {
