cb1a390ce671f8bad6c531d06683687788d2a987
Sudoer <sudoer.zero@gmail.com>
2022-05-08 02:59:14 +0200

    Create a project model and register to admin.

--- work/models.py
+++ work/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    gh_url = models.URLField()
+    status = models.CharField(max_length=100, blank=True)
+    prog_lang = TaggableManager()
+
+    def __str__(self):
+        return self.name
